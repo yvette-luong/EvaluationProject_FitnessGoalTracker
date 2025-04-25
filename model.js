93539559a37dcc9e0f1458096d56c17cd15ae51c
Hang Luong - Yvette <63530290+yvette-luong@users.noreply.github.com>
2025-04-25 14:59:49 -0700

    fixed model issue. add function works

--- model.js
+++ model.js
@@ -1,33 +1,37 @@
+// import { APIs } from './api.js'; 
 
 export const Model = (() => {
-    class State {
-        #goals = [];
-        #onChange = () => {};
-    
-        get goals() {
-          return this.#goals;
-        }
-    
-        set goals(newGoals) {
-          this.#goals = newGoals;
-          this.#onChange();
-        }
-    
-        addGoal(newGoal) {
-          this.goals = [...this.goals, newGoal];
-        }
-    
-        deleteGoal(id) {
-          this.goals = this.goals.filter((goal) => goal.id !== id);
-        }
-    
-        subscribe(cb) {
-          this.#onChange = cb;
-        }
-      }
-      const getGoals = () => APIs.getGoals();
-      const createGoal = (newGoal) => APIs.createGoal(newGoal);
-      const deleteGoal = (id) => APIs.deleteGoal(id);
+  class State {
+    #goals = [];
+    #onChange = () => {};
 
-      return {State, getGoals, createGoal, deleteGoal}
-})
+    get goals() {
+      return this.#goals;
+    }
+
+    set goals(newGoals) {
+      this.#goals = newGoals;
+      this.#onChange();
+    }
+
+    addGoal(newGoal) {
+      this.goals = [...this.goals, newGoal];
+    }
+
+    deleteGoal(id) {
+      this.goals = this.goals.filter((goal) => goal.id !== id);
+    }
+
+    subscribe(cb) {
+      this.#onChange = cb;
+    }
+  }
+
+  const getGoals = async () => await APIs.getGoals();
+  const createGoal = async (newGoal) => await APIs.createGoal(newGoal);
+  const deleteGoal = async (id) => await APIs.deleteGoal(id);
+
+  const modelObject = { State, getGoals, createGoal, deleteGoal };
+  console.log('Model inside model.js:', modelObject); 
+  return modelObject;
+})();
